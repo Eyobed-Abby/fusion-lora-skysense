edd3075773839a6400bc427a90893a4914ba42aa
Winner <79535526+Eyobed-Abby@users.noreply.github.com>
2025-11-17 00:42:27 +0400

    Update __init__.py

--- external/skysense_o/__init__.py
+++ external/skysense_o/__init__.py
@@ -1,3 +1,19 @@
-from skysense_o.data import build_detection_test_loader, build_detection_train_loader
-from skysense_o import SkySenseODatasetMapper
-from skysense_o.skysense_o_model import SkySenseO
+# from skysense_o.data import build_detection_test_loader, build_detection_train_loader
+# from skysense_o import SkySenseODatasetMapper
+# from skysense_o.skysense_o_model import SkySenseO
+
+# external/skysense_o/__init__.py
+
+"""
+Lightweight init for the vendored SkySense-O package.
+
+We only need the modules to be importable; any environment-specific
+paths (HPC, DETECTRON2_DATASETS, etc.) are handled outside.
+"""
+
+from . import data
+from . import modeling
+from .skysense_o_model import SkySenseO
+from .data.dataset_mappers.skysense_o_dataset_mapper import SkySenseODatasetMapper
+
+__all__ = ["data", "modeling", "SkySenseO", "SkySenseODatasetMapper"]
